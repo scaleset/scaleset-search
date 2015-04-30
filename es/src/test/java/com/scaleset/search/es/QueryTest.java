ad50b3f59af67ac3fe26d45820493f2d1fac4155
Christopher Lakey <christopherlakey@gmail.com>
2015-04-30 16:04:22 -0400

    update scroll to keepAlive in unit test

--- es/src/test/java/com/scaleset/search/es/QueryTest.java
+++ es/src/test/java/com/scaleset/search/es/QueryTest.java
@@ -93,7 +93,7 @@ public class QueryTest extends Assert {
 
     @Test
     public void testScrolling() throws Exception {
-        QueryBuilder queryBuilder = new QueryBuilder().limit(1).header("scroll", "1m");
+        QueryBuilder queryBuilder = new QueryBuilder().limit(1).header("keepAlive", "1m");
         Query query = queryBuilder.build();
         Results<Feature> results = featureDao.search(query);
         int retrieved = 0;
