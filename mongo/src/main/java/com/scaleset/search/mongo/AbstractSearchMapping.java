b0cfd63996e8d0ce0b1e48adf9da876f3d3b324c
Christopher Lakey <christopherlakey@gmail.com>
2015-05-13 11:17:28 -0400

    Add Java 8 function interfaces to Mongo abstract search mapping to simplify implementation

--- mongo/src/main/java/com/scaleset/search/mongo/AbstractSearchMapping.java
+++ mongo/src/main/java/com/scaleset/search/mongo/AbstractSearchMapping.java
@@ -6,6 +6,10 @@ import com.mongodb.BasicDBObject;
 import com.mongodb.DBObject;
 import com.scaleset.geo.geojson.GeoJsonModule;
 import com.scaleset.search.Query;
+import com.scaleset.utils.Coerce;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.function.Function;
 
 public abstract class AbstractSearchMapping<T, K> implements SearchMapping<T, K> {
 
@@ -13,6 +17,24 @@ public abstract class AbstractSearchMapping<T, K> implements SearchMapping<T, K>
     private ObjectMapper objectMapper = new ObjectMapper().registerModule(new GeoJsonModule());
     private JavaType javaType;
     private SchemaMapper schemaMapper = new SimpleSchemaMapper("text");
+    private Function<K, String> keyMapper;
+    private Function<T, String> idMapper;
+
+    public AbstractSearchMapping(String defaultCollection) {
+        this.defaultCollection = defaultCollection;
+        Class<T> persistentClass = (Class<T>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[0];
+        this.javaType = objectMapper.getTypeFactory().constructType(persistentClass);
+    }
+
+    public AbstractSearchMapping(String defaultCollection, Function<T, String> idMapper, Function<K, String> keyMapper) {
+        this.defaultCollection = defaultCollection;
+        Class<T> persistentClass = (Class<T>) ((ParameterizedType) getClass()
+                .getGenericSuperclass()).getActualTypeArguments()[0];
+        this.javaType = objectMapper.getTypeFactory().constructType(persistentClass);
+        this.idMapper = idMapper;
+        this.keyMapper = keyMapper;
+    }
 
     public AbstractSearchMapping(Class<? extends T> type, String defaultCollection) {
         this(type, defaultCollection, new SimpleSchemaMapper("text"));
@@ -46,12 +68,45 @@ public abstract class AbstractSearchMapping<T, K> implements SearchMapping<T, K>
     }
 
     @Override
-    public abstract String id(T obj) throws Exception;
+    public String id(T obj) throws Exception {
+        if (idMapper != null) {
+            return idMapper.apply(obj);
+        } else {
+            throw new RuntimeException("No idMapper registered");
+        }
+    }
 
     @Override
-    public abstract String idForKey(K key) throws Exception;
+    public String idForKey(K key) throws Exception {
+        if (keyMapper != null) {
+            return keyMapper.apply(key);
+        } else {
+            return Coerce.toString(key);
+        }
+    }
+
+    protected AbstractSearchMapping<T, K> idMapper(Function<T, String> idMapper) {
+        this.idMapper = idMapper;
+        return this;
+    }
+
+    protected AbstractSearchMapping<T, K> keyMapper(Function<K, String> keyMapper) {
+        this.keyMapper = keyMapper;
+        return this;
+    }
+
+    protected AbstractSearchMapping<T, K> objectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        return this;
+    }
+
+    protected AbstractSearchMapping<T, K> schemaMapper(SchemaMapper schemaMapper) {
+        this.schemaMapper = schemaMapper;
+        return this;
+    }
 
     @Override
+
     public DBObject toDocument(T obj) throws Exception {
         DBObject result = objectMapper.convertValue(obj, BasicDBObject.class);
         return result;
@@ -69,4 +124,5 @@ public abstract class AbstractSearchMapping<T, K> implements SearchMapping<T, K>
     protected SchemaMapper getSchemaMapper() {
         return schemaMapper;
     }
+
 }
